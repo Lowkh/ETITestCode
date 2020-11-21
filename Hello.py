30f1b3f45f67d78cb3ce386c6af2ea42c76582aa
Lowkh <54532616+Lowkh@users.noreply.github.com>
2020-11-21 10:10:10 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,10 +2,10 @@ print("Hello!")
 
 print("This is the addon for feature 1")
 
-print("i am adding this line!!!")//feature 1
+print("i am adding this line!!!")
 
 print("Hello this is adding new feature1")
 
-print("add new item") //fix the new bug// change for bug
+print("add new item")
 
 
