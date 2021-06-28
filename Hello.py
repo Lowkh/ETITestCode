8a9e50da589dac4605fee5ce5d8bd576564081f2
Lowkh <54532616+Lowkh@users.noreply.github.com>
2021-06-28 18:22:53 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -8,7 +8,7 @@ print("This is the addon for feature 1")
 
 print("i am adding this line!!!")
 
-print("Hello this is adding new feature1")
+print("Hello this is adding new feature1")  //
 
 #add new comment //
 
